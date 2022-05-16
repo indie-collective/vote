e43eec47847de9c68d62411e601230596d337f8b
Christopher Humphries <engleek@gmail.com>
2022-05-16 22:55:58 +0200

    Dev db

--- app/utils/db.server.js
+++ app/utils/db.server.js
@@ -1,5 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const db = new PrismaClient();
+let db;
+
+if (process.env.NODE_ENV === "production") {
+  db = new PrismaClient();
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+  }
+  db = global.__db;
+}
 
 export { db };
